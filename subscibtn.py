5962203f7940b5ef5dfde032d86abd51144bad00
romil007 <46789613+romil007@users.noreply.github.com>
2019-03-25 12:58:45 +0530

    Project of web scrapping/data manipulation
    This web scrapping project is done on python. The data is extracted from of tatasky channel recharge site.
    This project is about to get data from existing website and do data operations.
    Libraries used are BeautifulSoup, Requests, Urlib, RE etc.
    Details: Using these libraries all channels data is taken and stored in dataframe. First site response is catched, then it is transformed into text format, which is then mined to get useful containers where channel names and details are present. Futher it is formed in to list and shown as dataframe
    Vision: By selecting channels by user it should recommend the channels packs with price.

---
+++ subscibtn.py
@@ -0,0 +1,123 @@
+#Python Project related to web scrapping
+'''In this project input is to be given of which channel do you want and 
+   it will in return give recommendation of which pack is vmost useful and viable.
+   
+'''
+from bs4 import BeautifulSoup
+#from urllib.request import urlopen,Request
+import requests
+import pandas as pd
+import re
+import numpy as np
+url = 'https://www.tatasky.com/wps/portal/TataSky/packs/tata-sky-channels'
+
+'''request=Request(url)
+response=urlopen(request)
+
+# This is used as old method'''
+
+# Packages the request, send the request and catch the response in variable 'channel_list'
+channel_list=requests.get(url)
+
+# Extract the response: text
+text=channel_list.text
+
+#print(text)
+# If we print whole code, might take time and memory so just take some part to check.
+#print(channel_list.text[:500])
+
+#Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(text)
+type(soup)
+print(soup)
+# Prettify the BeautifulSoup object: pretty_soup
+pretty_soup=soup.prettify()
+print(type(pretty_soup))
+print(pretty_soup[:500])
+
+'''# Get the title 
+tatasky_title=pretty_soup.title
+print(tatasky_title)'''
+# 'str' object has no attribute 'get_text'
+#asd=pretty_soup.get_text()
+'''# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags=soup.find_all('a')
+#print(a_tags)
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get('href'))'''
+#Parse response.text by creating a BeautifulSoup object, and assign this object to html_soup. 
+#The 'html.parser' argument indicates that we want to do the parsing using Python’s built-in HTML parser.
+
+#Now let's use the find_all() method 
+#to extract all the div containers that have a class attribute of lister-item mode-advanced:
+
+channel_containers =soup.find_all('div', class_ ="card pack-details")
+
+print(channel_containers[0])
+channel_details=channel_containers[2]
+channel_details
+print(channel_details.div.h2.text)
+
+channel_priceconatainer = soup.find('span',class_="price-sm")
+channel_priceconatainer
+#channel_priceconatainer.text
+#type(channel_priceconatainer)
+#asd=channel_priceconatainer.text
+#asda=float(asd)
+#asda
+#type(asda)
+#type(asd)
+
+#print(channel_priceconatainer[2])
+
+
+import re
+
+#to solve by removing floats from span text
+#eg 
+'''for m in channel_price:
+    if m ==' ₹ 0 / month':
+        print('yes')'''
+# Lists to store the scraped data in
+channel_lists=[]
+channel_price=[]
+
+# Extract data from individual movie container
+
+for i in channel_containers:
+    #print(i)
+    
+    a=i.div.h2.text
+    channel_lists.append(a)
+    zxc=[]
+    b=i.span.text
+    c=re.findall(r"[-+]?\d*\.\d+|\d+", b)
+    
+ # since c is list + string value and not no. , convert to float  , but dataframe will be float bydefault.
+    d=float(c[0])
+    if d==float and d!=0.0:
+        pass
+    elif d==0.0:
+        d=0
+    channel_price.append(d)
+
+ 
+    
+print(channel_lists[10:20])
+(print(channel_price))
+
+test_sets = pd.DataFrame({'channels':channel_lists,'price':channel_price})
+
+print(test_sets.info())
+
+
+test_sets.head()
+
+
+
+
+
+
+
+
